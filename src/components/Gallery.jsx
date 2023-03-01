bf4b27df541ef0cf206d5dd4f5cd1e851052dd7a
Alexis Pennel <alexispennel@gmail.com>
2023-03-01 03:57:50 +0100

    design gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,19 @@
 import styles from './Gallery.module.css'
+import arrowLeft from '../images/arrowLeft.svg';
+import arrowRight from '../images/arrowRight.svg';
+
 
 const Gallery = (props) => {
     const pictures = props.pictures;
 
     return (
-        <section className={styles.Gallery__box}>
+        <section className={styles.gallery__box}>
             <img src={pictures[0]} alt="" />
+            <div className={styles.gallery__arrows__box}>
+                <img src={arrowLeft} alt="" />
+                <img src={arrowRight} alt="" />
+            </div>
+            <p className={styles.gallery__targetPoints}>1/4</p>
         </section>
     )
 };
